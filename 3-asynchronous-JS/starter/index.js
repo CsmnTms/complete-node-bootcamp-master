b9d44aebffaded0213be852cc93f1792d8f6808d
Tamas Cosmin <csmntamas97@gmail.com>
2025-05-10 15:13:56 +0300

    section5_building-promises

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -1,20 +1,40 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-fs.readFile(`${__dirname}/dog.txt`, "utf-8", (err, data) => {
-  console.log(`Breed: ${data}`);
-
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile("dog-img.txt", res.body.message, (err) => {
-        if (err) return console.log(err.message);
-        console.log("Random dog image saved to file!");
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
+const readFilePromise = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf-8", (err, data) => {
+      if (err) reject("I could not find that file");
+      resolve(data);
     });
-});
+  });
+}
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject("Could not write to file");
+      resolve("File written successfully");
+    });
+  });
+}
+
+// notice the chain of promises
+// each promise returns another promise
+// so we can chain them together
+readFilePromise(`${__dirname}/dog.txt`)
+  .then(data => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+  })
+  .then((response) => {
+    console.log(response.body.message);
+    return writeFilePromise("dog-img.txt", response.body.message);
+  })
+  .then(result => {
+    console.log(result);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+

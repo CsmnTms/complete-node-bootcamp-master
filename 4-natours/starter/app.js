17c41ad7984357b8cc602726eaa99a523c5aed93
Tamas Cosmin <csmntamas97@gmail.com>
2025-05-11 01:07:47 +0300

    section6_starting-the-api-handling-get-requests

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,13 +1,27 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.status(200).json({ message: 'Hello from the server!', app: 'Natours' });
-});
+// app.get('/', (request, response) => {
+//   response.status(200).json({ message: 'Hello from the server!', app: 'Natours' });
+// });
 
-app.post('/', (request, response) => {
-  response.status(200).json({ message: 'You can post to this endpoint', app: 'Natours' });
+// app.post('/', (request, response) => {
+//   response.status(200).json({ message: 'You can post to this endpoint', app: 'Natours' });
+// });
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8'));
+
+// this is called a route handler:
+app.get('/api/v1/tours', (request, response) => {
+  response.status(200).json({
+    status: 'success',
+    resultCount: toursObj.length,
+    data: {
+      tours
+    },
+  });
 });
 
 const port = 420;

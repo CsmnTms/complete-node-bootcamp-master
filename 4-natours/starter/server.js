5d684f6f862a04c92ece18c6fee96a7720bb8f25
Tamas Cosmin <csmntamas97@gmail.com>
2025-05-12 00:22:29 +0300

    section8_creating-documents-and-testing-the-model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -28,6 +28,20 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997,
+});
+
+testTour
+.save()
+.then((doc) => {
+  console.log(doc);
+})
+.catch((err) => {
+  console.log('Error saving tour:', err);
+});
+
 // START SERVER
 const port = process.env.PORT || 420;
 app.listen(port, () => {

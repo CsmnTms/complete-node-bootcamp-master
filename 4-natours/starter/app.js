616eba72f1365ff42ee3dc692c5448d2493e3955
Tamas Cosmin <csmntamas97@gmail.com>
2025-05-11 15:31:17 +0300

    section6_environment-variables

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,8 @@
+/////////////////////////////////////////////////////////////////////////////////
+/// This is used to configure everything that has to do with the express app. ///
+///    More general node.JS related stuff is found in the server.js file.     ///
+/////////////////////////////////////////////////////////////////////////////////
+
 // Third-party modules
 const express = require('express');
 const morgan = require('morgan');
@@ -9,13 +14,17 @@ const userRouter = require('./routes/userRoutes');
 // App startup
 const app = express();
 
-// Middlewares
-app.use(morgan('dev')); // logging middleware, logs all requests to the console
+// MIDDLEWARES
+console.log(process.env.NODE_ENV); // this is the node process env vars
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev')); // logging middleware, logs all requests to the console
+}
+
 app.use(express.json()); // parses incoming JSON requests and puts the parsed data in req.body, otherwise no body is received
 app.use(express.static(`${__dirname}/public`)); // serves static files from the public directory
 
 app.use((request, response, next) => {
-  console.log("Hello from the middleware");
+  // console.log("Hello from the middleware");
   next(); // this is important, otherwise the request will hang
 });
 app.use((request, response, next) => {
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,12 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' }); // this loads the env vars from the config.env file into process.env
 const app = require ('./app');
 
+// console.log("app.get('env'): :", app.get('env')); // this is the express app env mode (development, production, etc)
+// console.log(process.env); // this is the node process env vars
+
 // START SERVER
-const port = 420;
+const port = process.env.PORT || 420;
 app.listen(port, () => {
   console.log(`Server is running http://localhost:${port}...`);
 });

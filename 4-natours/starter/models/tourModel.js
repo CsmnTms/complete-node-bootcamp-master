9f0044b93ad17be7cfe3ac730bb119f43c1cc017
Tamas Cosmin <csmntamas97@gmail.com>
2025-05-15 02:47:12 +0300

    section8_extending-the-tours-model

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -5,15 +5,56 @@ const tourSchema = new Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'hard ;)'],
+      message: 'Difficulty is either: easy, medium, or hard ;)'
+    },
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   price: {
     type: Number,
-    required: [true, 'A tour must have a price'],
+    required: [true, 'A tour must have a price']
   },
-  rating: {
-    type: Number,
-    default: 4.5,
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary']
   },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 });
 
 const Tour = model('Tour', tourSchema);

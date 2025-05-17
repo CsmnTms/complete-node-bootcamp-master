feedb7ea0c1016f5b0d426a22a93141733eb24e5
Tamas Cosmin <csmntamas97@gmail.com>
2025-05-17 12:58:49 +0300

    section8_refactoring-api-features

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -1,51 +1,18 @@
-import qs from 'qs';
 import Tour from '../models/tourModel.js';
+import { APIFeatures } from '../utils/apiFeatures.js';
 
 export { aliasTopTours, getAllTours, getTour, createTour, patchTour, deleteTour };
 
 async function getAllTours(request, response) {
   try {
-    // BUILD QUERY
-    const queryObj = qs.parse(request._parsedUrl.query);
-    const excludedFields = ['page', 'sort', 'limit', 'fields']; 
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // Filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting
-    if (request._parsedUrl.query.sort) {
-      const sortBy = request._parsedUrl.query.sort.split(',').join(' ');
-      query = query.sort(sortBy);
-    } else {
-      query.sort('-createdAt');
-    }
-
-    // Field limiting / query projection
-    if (request._parsedUrl.query.fields) {
-      const fields = request._parsedUrl.query.fields.split(',').join(' ');
-      query = query.select(fields);
-    } else {
-      query.select('-__v');
-    }
-
-    // Pagination
-    const page = request._parsedUrl.query.page * 1 || 1;
-    const limit = +request._parsedUrl.query.limit || 100;
-    const skip = (page - 1) * limit;
-
-    query = query.skip(skip).limit(limit);  
-
-    if (request._parsedUrl.query.page) {
-      const numTours = await Tour.countDocuments();
-      if (skip >= numTours) throw new Error('This page does not exist');
-    }
-
     // EXECUTE QUERY
-    const tours = await query
+    const features = new APIFeatures(Tour.find(), request._parsedUrl.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const tours = await features.query;
 
     // SEND RESPONSE
     response.status(200).json({
@@ -69,7 +36,6 @@ async function aliasTopTours(request, response, next) {
   request._parsedUrl.query.limit = '5';
   request._parsedUrl.query.fields = 'name,price,ratingsAverage,difficulty';
 
-  // console.log(request);
   console.log(request._parsedUrl.query.sort);
 
   next();
@@ -96,7 +62,6 @@ async function getTour(request, response) {
 
 async function createTour(request, response) {
   try {
-    console.log(request.body);
     const newTour = await Tour.create(request.body);
 
     response.status(201).json({

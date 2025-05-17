57f81233b72e584ef4cf5e2ab6bc22f9b0b594a8
Tamas Cosmin <csmntamas97@gmail.com>
2025-05-17 19:24:54 +0300

    section8_aggregation-middleware

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -95,8 +95,15 @@ tourSchema.pre(/^find/, function(next) {
 });
 
 tourSchema.post(/^find/, function(docs, next) {
-  console.log('Query executed:', docs);
+  // console.log('Query executed:', docs);
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+// AGGREGATION MIDDLEWARE: runs pre- / post- .aggregate()
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { isSecret: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 

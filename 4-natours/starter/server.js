9238b0b9d0da1225ebc9c6ee095b8780da331ad4
Tamas Cosmin <csmntamas97@gmail.com>
2025-05-20 01:21:13 +0300

    section9_debugging

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -6,6 +6,7 @@ connect(process.env.DATABASE).then(() => {
   console.log('///_________________________ DB connection successful! _________________________///');
 });
 
+//await new Promise(resolve => setTimeout(resolve, 5000));
 // START SERVER
 const port = process.env.PORT || 420;
 app.listen(port, () => {
